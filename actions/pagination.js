55011ff2c7063965fd4ce3b80c2f34ea44843dc6
yugo_i <u15jpn@gmail.com>
2020-12-07 15:50:02 +0900

    styling improvement

--- actions/pagination.js
+++ actions/pagination.js
@@ -39,7 +39,7 @@ export const useGetBlogsPages = ({ blogs, filter }) => {
                 <CardListItemBlank />
               </Col>
             ) : (
-              <Col key={`${i}-item`} md="4">
+              <Col key={`${i}-item`} md="6" lg="4">
                 <CardItemBlank />
               </Col>
             )
@@ -62,7 +62,7 @@ export const useGetBlogsPages = ({ blogs, filter }) => {
             />
           </Col>
         ) : (
-          <Col key={blog.slug} md="4">
+          <Col key={blog.slug} md="6" lg="4">
             <CardItem
               author={blog.author}
               title={blog.title}
--- components/CardItem.js
+++ components/CardItem.js
@@ -62,8 +62,14 @@ const CardItem = ({
             </>
           ) : (
             <>
-              <Card.Title className="card-main-title">{title}</Card.Title>
-              <Card.Text>{subtitle}</Card.Text>
+              <Card.Title className="card-main-title">
+                {title.length > 40 ? title.substr(0, 40) + "..." : title}
+              </Card.Title>
+              <Card.Text>
+                {subtitle.length > 40
+                  ? subtitle.substr(0, 40) + "..."
+                  : subtitle}
+              </Card.Text>
             </>
           )}
         </Card.Body>
